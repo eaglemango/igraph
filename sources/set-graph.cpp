fe9a33f1710873c4e18ad94138180bea720f0c9e
Sergey Gorbunov <eaglemango@gmail.com>
2019-02-20 16:54:38 +0300

    Create set-graph.cpp

---
+++ sources/set-graph.cpp
@@ -0,0 +1,45 @@
+#include "../headers/set-graph.h"
+
+SetGraph::SetGraph(int vertices_count) {
+    children.resize(static_cast<unsigned long>(vertices_count));
+}
+
+SetGraph::SetGraph(const IGraph* old_graph) {
+    children.resize(static_cast<unsigned long>(old_graph->VerticesCount()));
+
+   std::vector<int> vertices;
+   for (int from = 0; from < old_graph->VerticesCount(); ++from) {
+       old_graph->GetNextVertices(from, vertices);
+       for (int to : vertices) {
+           AddEdge(from, to);
+       }
+   }
+}
+
+SetGraph::~SetGraph() {}
+
+void SetGraph::AddEdge(int from, int to) {
+    children[from].insert(to);
+}
+
+int SetGraph::VerticesCount() const {
+    return static_cast<int>(children.size());
+}
+
+void SetGraph::GetNextVertices(int vertex, std::vector<int>& vertices) const {
+    vertices.clear();
+
+    for (int to : children[vertex]) {
+        vertices.push_back(to);
+    }
+}
+
+void SetGraph::GetPrevVertices(int vertex, std::vector<int>& vertices) const {
+    vertices.clear();
+
+    for (int from = 0; from < VerticesCount(); ++from) {
+        if (children[from].find(vertex) != children[from].end()) {
+            vertices.push_back(from);
+        }
+    }
+}
