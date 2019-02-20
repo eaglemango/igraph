7a1e578e112e93a3244b8e520bdf16734f562ee1
Sergey Gorbunov <eaglemango@gmail.com>
2019-02-20 16:47:10 +0300

    Create set-graph.h

---
+++ headers/set-graph.h
@@ -0,0 +1,18 @@
+#include "igraph.h"
+#include <unordered_set>
+
+class SetGraph : public IGraph {
+public:
+    SetGraph(int vertices_count);
+    SetGraph(const IGraph* old_graph);
+    ~SetGraph() override;
+
+    void AddEdge(int from, int to) override;
+
+    int VerticesCount() const override;
+
+    void GetNextVertices(int vertex, std::vector<int>& vertices) const override;
+    void GetPrevVertices(int vertex, std::vector<int>& vertices) const override;
+private:
+    std::vector<std::unordered_set<int>> children;
+};
