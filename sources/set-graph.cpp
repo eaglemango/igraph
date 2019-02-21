8d44129ba779c2739c91caa550249be0ea21fa55
Sergey Gorbunov <eaglemango@gmail.com>
2019-02-21 22:20:40 +0300

    Update set-graph.cpp

--- sources/set-graph.cpp
+++ sources/set-graph.cpp
@@ -16,7 +16,37 @@ SetGraph::SetGraph(const IGraph* old_graph) {
    }
 }
 
-SetGraph::~SetGraph() {}
+SetGraph::~SetGraph() = default;
+
+SetGraph::SetGraph(const SetGraph& set_graph) {
+    children.resize(static_cast<unsigned long>(set_graph.VerticesCount()));
+
+    std::vector<int> vertices;
+    for (int from = 0; from < set_graph.VerticesCount(); ++from) {
+        set_graph.GetNextVertices(from, vertices);
+        for (int to : vertices) {
+            AddEdge(from, to);
+        }
+    }
+}
+
+SetGraph& SetGraph::operator=(const SetGraph& set_graph) {
+    if (this == &set_graph) {
+        return *this;
+    }
+
+    children.resize(static_cast<unsigned long>(set_graph.VerticesCount()));
+
+    std::vector<int> vertices;
+    for (int from = 0; from < set_graph.VerticesCount(); ++from) {
+        set_graph.GetNextVertices(from, vertices);
+        for (int to : vertices) {
+            AddEdge(from, to);
+        }
+    }
+
+    return *this;
+}
 
 void SetGraph::AddEdge(int from, int to) {
     children[from].insert(to);
