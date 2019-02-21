a52a57e99a038b0a6884569c6d960705a2a27f14
Sergey Gorbunov <eaglemango@gmail.com>
2019-02-21 22:05:55 +0300

    Update list-graph.h

--- headers/list-graph.h
+++ headers/list-graph.h
@@ -6,7 +6,7 @@ public:
     ListGraph(const IGraph* old_graph);
     
     ~ListGraph() override;
-    ListGraph(const ListGraph& another_graph);
+    ListGraph(const ListGraph& list_graph);
     ListGraph& operator=(const ListGraph& list_graph);
 
     void AddEdge(int from, int to) override;
